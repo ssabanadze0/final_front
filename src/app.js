bd8f163767dc4d51d67ef8f9ae086ee193214aad
ssabanadze0 <ssabanadze0@gmail.com>
2025-07-13 17:37:49 +0400

    abdaubda

---
+++ src/app.js
@@ -0,0 +1,18 @@
+// // 1slider
+// const images = [
+//   "src/images/you-have-choices.PNG",
+//   "src/images/its-tricky-to-choose.png",
+//   "src/images/what-you-ought.png",
+//   "src/images/what-makes-you-ill.png",
+//   "src/images/everything-will.png",
+// ];
+
+// let currentIndex = 0;
+// const sliderImage = document.getElementById("slider-image");
+
+// function changeImage() {
+//   currentIndex = (currentIndex + 1) % images.length;
+//   sliderImage.src = images[currentIndex];
+// }
+
+// setInterval(changeImage, 5000);
